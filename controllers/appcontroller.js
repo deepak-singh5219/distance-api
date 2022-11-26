b6e01c871f67ad3edeb2f852d6393ed185d975af
Deepak Singh <singhdeepak5219@gmail.com>
2022-11-27 00:34:12 +0530

    api call

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -1,8 +1,15 @@
-var request = require('request');
+const axios = require('axios');
+const CircularJSON = require('circular-json');
 
-exports.getNearby = (req,res) => {
+exports.getNearby = async (req,res) => {
 
-   
+    const details = req.body
     
 
+   const data = await axios.post('https://monkfish-app-29mfl.ondigitalocean.app/',details);
+   console.log(data);
+        res.status(200).json({
+            "message":"success",
+            "response":data.data
+        })
 }
